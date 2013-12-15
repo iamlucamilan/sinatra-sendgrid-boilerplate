1c2a352d1b00920680544327d15b5b694ada7576
Luca Milan <milan.luca@gmail.com>
2013-12-15 19:21:15 +0100

    Added some changes to email sender

--- app.rb
+++ app.rb
@@ -7,17 +7,21 @@ end
 
 post '/contact' do
   require 'pony'
+
   Pony.mail({
     :to => 'milan.luca@gmail.com',
+    :subject => 'Contatto da sito fabioegiuliasisposano.it',
+    :body => "From #{params[:name]} - #{params[:email]}:  #{params[:text]}",
     :via => :smtp,
     :via_options => {
       :address              => 'smtp.gmail.com',
       :port                 => '587',
       :enable_starttls_auto => true,
-      :user_name            => 'milan.luca',
-      :password             => 'Nqdce265123',
+      :user_name            => 'scquizzatoconlacq',
+      :password             => 'ottorino1957',
       :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
       :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-    }
+    },
+    :from => "#{params[:email]}"
   })
 end
