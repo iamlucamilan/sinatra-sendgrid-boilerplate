02d284bec13ca04efbdad7d9e92056f6bb57532b
Luca Milan <milan.luca@gmail.com>
2013-12-16 20:16:25 +0100

    Updated app with sendgrid installation

--- app.rb
+++ app.rb
@@ -10,15 +10,15 @@ post '/contact' do
     :subject => 'Contatto da sito fabioegiuliasisposano.it',
     :body => "From #{params[:name]} - #{params[:email]}:  #{params[:text]}",
     :via => :smtp,
-    # :via_options => {
-    #   :address              => 'smtp.gmail.com',
-    #   :port                 => '587',
-    #   :enable_starttls_auto => true,
-    #   :user_name            => 'scquizzatoconlacq',
-    #   :password             => 'ottorino1957',
-    #   :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    #   :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-    # },
+    :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'heroku.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
     :from => "#{params[:email]}"
   })
 end
