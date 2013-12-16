31b84c2649e52b21383c9c873c6c59d4c7847ba7
Luca Milan <milan.luca@gmail.com>
2013-12-16 14:53:37 +0100

    removed require directive

--- app.rb
+++ app.rb
@@ -1,27 +1,24 @@
 # app.rb
-require 'sinatra'
 
 get '/' do
   erb :index
 end
 
 post '/contact' do
-  require 'pony'
-
   Pony.mail({
     :to => 'milan.luca@gmail.com',
     :subject => 'Contatto da sito fabioegiuliasisposano.it',
     :body => "From #{params[:name]} - #{params[:email]}:  #{params[:text]}",
     :via => :smtp,
-    :via_options => {
-      :address              => 'smtp.gmail.com',
-      :port                 => '587',
-      :enable_starttls_auto => true,
-      :user_name            => 'scquizzatoconlacq',
-      :password             => 'ottorino1957',
-      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-      :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-    },
+    # :via_options => {
+    #   :address              => 'smtp.gmail.com',
+    #   :port                 => '587',
+    #   :enable_starttls_auto => true,
+    #   :user_name            => 'scquizzatoconlacq',
+    #   :password             => 'ottorino1957',
+    #   :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    #   :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    # },
     :from => "#{params[:email]}"
   })
 end
